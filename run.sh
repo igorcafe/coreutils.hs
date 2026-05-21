9f5d55658d7ba7f9cc3e7309bf5f8df23b01321b
Igor Cafe <igorcafe@fake.com>
2026-05-21 17:13:13 -0300

    ghc: better warning/error

--- run.sh
+++ run.sh
@@ -6,7 +6,15 @@ echo "> cleaning up" >&2
 rm -f coreutils
 
 echo "> compiling..." >&2
-ghc -no-keep-hi-files -no-keep-o-files -Wincomplete-patterns -Werror Main.hs -o coreutils
+ghc \
+    -no-keep-hi-files \
+    -no-keep-o-files \
+    -Wall \
+    -Werror \
+    -Wno-missing-signatures \
+    -Wno-error=unused-imports \
+    Main.hs \
+    -o coreutils
 
 name="${1:-coreutils}"
 shift
